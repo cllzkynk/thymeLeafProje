006df2a01cfe88d660032814d2a3e4a6b861a665
cllzkynk <cllzkynk@gmail.com>
2022-02-13 02:08:00 +0200

    first commit

---
+++ src/main/java/thymeleaf3/ThymeLeafProjeApplication.java
@@ -0,0 +1,13 @@
+package thymeleaf3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafProjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeLeafProjeApplication.class, args);
+	}
+
+}
